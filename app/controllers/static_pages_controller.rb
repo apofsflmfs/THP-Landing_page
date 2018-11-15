c7d1d5fe488a20b834084553264541587360a9fd
Clem Baldy <clemoun@yahoo.fr>
2018-11-15 14:23:53 +0100

    MailChimp ok to save visitor emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+
   end
+
+  def subscribe
+    ActionMailchimp.new.subscribe(params["email"], params["targeted_customer"])
+    flash[:notice] = "Vous êtes abonné à la newsletter!"
+    redirect_to "/"
+  end
+
 end
