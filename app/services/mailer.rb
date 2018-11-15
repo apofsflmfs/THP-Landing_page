7e1683a95d8e61e29d269b5c1130cd284aa4c6d6
Clem Baldy <clemoun@yahoo.fr>
2018-11-15 18:10:34 +0100

    changements divers

--- app/services/mailer.rb
+++ app/services/mailer.rb
@@ -1,21 +1,17 @@
-require 'rubygems'
-require 'mail'
-
-
-class Gmail
+class Mailer
 
   def send_mails(mail)
 
     username = 'montpellier.thp@gmail.com'
-    password ='&&&2018&&&'
+    password = Rails.application.credentials.GMAIL_PASSWORD
     adresse = mail
 
-    gmail = Gmail.connect(username,password)
+    gmail = Gmail.connect!(username,password)
     gmail.deliver do
       to adresse
       subject "Le projet THP propose d'apprendre d'apprendre gratuitement à programmer !"
       content_type 'text/html; charset=UTF-8'
-      body "<p>Bonjour, <br><br>"
+      body "<p>Bonjour, <br> Vous vous êtes bien abonné à la Newsletter de THP.<br>"
     end
     gmail.logout
   end
