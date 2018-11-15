33ff3c584f6cb6780cb22262dad1ae1d4e199e1e
Thierry <kasskq@gmail.com>
2018-11-15 13:51:49 +0100

    heroku 9eme essai

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("lib")
 Rails.application.config.assets.paths << Rails.root.join("vendor")
-Rails.application.config.assets.paths << Rails.root.join("vendor/js")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "js")
 
 
 
