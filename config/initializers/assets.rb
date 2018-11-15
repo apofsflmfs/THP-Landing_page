55d9238ed5bef25b4072b513331c049a98752b82
Thierry <kasskq@gmail.com>
2018-11-15 14:13:46 +0100

    premiere version html/css/js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("lib")
 Rails.application.config.assets.paths << Rails.root.join("vendor")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "js")
 
 
 
