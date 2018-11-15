7abdf8de12bc087385713cc80d13ef131deb0309
Clem Baldy <clemoun@yahoo.fr>
2018-11-15 21:27:35 +0100

    Débug Gmail

--- app/services/mailer.rb
+++ app/services/mailer.rb
@@ -1,18 +1,17 @@
 class Mailer
+  def send_email(email)
+    mail_subject = "Welcome to THP!!" 
+    gmail = Gmail.connect!("team.montpellier.thp@gmail.com",Rails.application.credentials.GMAIL_PASSWORD)
 
-  def send_mails(mail)
-
-    username = 'montpellier.thp@gmail.com'
-    password = Rails.application.credentials.GMAIL_PASSWORD
-    adresse = mail
-
-    gmail = Gmail.connect!(username,password)
     gmail.deliver do
-      to adresse
-      subject "Le projet THP propose d'apprendre d'apprendre gratuitement à programmer !"
-      content_type 'text/html; charset=UTF-8'
-      body "<p>Bonjour, <br> Vous vous êtes bien abonné à la Newsletter de THP.<br>"
+      to email
+      subject mail_subject
+      text_part do
+        body "Bonjour,
+        Le projet THP propose d'apprendre d'apprendre gratuitement à programmer !
+        Vous êtes bien inscrits à la newsletter"
+      end
     end
-    gmail.logout
   end
 end
+
