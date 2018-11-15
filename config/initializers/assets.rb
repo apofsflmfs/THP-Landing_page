f93e101c9af3d533f8aa41af2610c3d0a6ce5c60
Thierry <kasskq@gmail.com>
2018-11-15 12:35:22 +0100

    heroku create sans font

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,9 +10,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("lib")
 Rails.application.config.assets.paths << Rails.root.join("vendor")
 Rails.application.config.assets.paths << Rails.root.join("vendor/js")
-Rails.application.config.assets.paths << Rails.root.join("vendor/fonts")
-Rails.application.config.assets.paths << Rails.root.join("vendor/fonts/fontAwesome_icon_font")
-Rails.application.config.assets.paths << Rails.root.join("vendor/fonts/linea_icon_font")
 
 
 
