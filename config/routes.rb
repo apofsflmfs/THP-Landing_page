f73e2e889166c9df436e6f29f5f5a9b659b1fdc5
Thierry <kasskq@gmail.com>
2018-11-16 18:29:40 +0100

    creation controller + page error 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
   root 'static_pages#home'
   get '/montpellier', to: 'static_pages#montpellier'
   get '/jobseeker', to: 'static_pages#jobseeker'
   post '/subscribe', to: 'static_pages#subscribe', as: :subscribe_nl
   get '/signed', to: 'static_pages#signed'
+  get '/404' => 'errors#not_found'
 end
