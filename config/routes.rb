bf2c4cb9d77fb4bec0d5ab2968d8e08c702bcb5f
Thierry <kasskq@gmail.com>
2018-11-16 17:25:37 +0100

    liens operationnels

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/montpellier', to: 'static_pages#montpellier'
   get '/jobseeker', to: 'static_pages#jobseeker'
-  post '/signed', to: 'static_pages#subscribe', as: :subscribe_nl
+  post '/subscribe', to: 'static_pages#subscribe', as: :subscribe_nl
   get '/signed', to: 'static_pages#signed'
 end
